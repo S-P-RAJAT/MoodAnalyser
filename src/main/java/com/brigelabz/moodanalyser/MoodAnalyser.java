64e21b0a84189a85b39dc672d986b148e227c4f1
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-08 19:57:04 +0530

    ADDED: Method to return mood of string

---
+++ src/main/java/com/brigelabz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,17 @@
+package com.brigelabz.moodanalyser;
+
+import java.util.Locale;
+
+public class MoodAnalyser {
+
+    public String analyseMood(String message) {
+
+        if (message.toLowerCase().contains("sad")) {
+
+            return "SAD";
+        } else {
+
+            return "HAPPY";
+        }
+    }
+}
